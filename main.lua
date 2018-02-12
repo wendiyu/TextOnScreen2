b96347fca2a65df59e4c6d8406165cc562115765
Wendi Yu <s439178@ocsb.local>
2018-02-12 14:20:49 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--Created by: Wendi Yu
+--Created on: Feb 2018
+--
+-- This file change the background color, place my name on the screen, place a new image on the screen
+-----------------------------------------------------------------------------------------
+
+display.setDefault( "background", 0, 0.5, 0.5 )
